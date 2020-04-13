82a97a658af4f9f5abec22fded3282214a6bfbec
josh <joshua@marinacci.org>
2020-04-13 15:34:35 -0700

    implement entering webxr

--- src/ecsy/webxr.js
+++ src/ecsy/webxr.js
@@ -1,13 +1,38 @@
 import { Component, System, World } from '../../node_modules/ecsy/build/ecsy.module.js?module';
 import {FullscreenMode} from './fullscreen.js'
 
-export class WebXRMode extends Component {
+export class WebXRActive extends Component {
 }
 
 export class WebXRButton extends Component {
+    constructor() {
+        super();
+        this.currentSession = null
+    }
 }
 
 export class WebXRSystem extends System {
+    enterWebXR(ent) {
+        console.log('trying to enter webxr')
+        let state = ent.getMutableComponent(WebXRButton);
+        if(state.currentSession === null) {
+            navigator.xr.requestSession('immersive-vr').then(session=>{
+                console.log("session has started")
+                let onSessionEnded = () => {
+                    console.log("session ended")
+                    state.currentSession.removeEventListener('end',onSessionEnded)
+                    renderer.vr.setSession(null)
+                    ent.removeComponent(WebXRActive)
+                }
+                session.addEventListener('end',onSessionEnded)
+                renderer.vr.setSession(session)
+                state.currentSession = session
+                ent.addComponent(WebXRActive)
+            })
+        } else {
+            state.currentSession.end()
+        }
+    }
     execute(delta, time) {
         this.queries.buttons.added.forEach(ent => {
             console.log("button added");
@@ -17,25 +42,19 @@ export class WebXRSystem extends System {
             elem.addEventListener('click', (e) => {
                 e.stopPropagation()
                 e.preventDefault()
-                ent.addComponent(WebXRButton)
+                this.enterWebXR(ent);
             })
             elem.disabled = true
             document.documentElement.append(elem)
             console.log("added the element", elem)
 
-            if ('xr' in navigator) {
-                console.log("has webxr")
-                elem.disabled = false
-                /*
-                navigator.xr.requestDevice().then((device) => {
-                    device.supportsSession({immersive: true})
-                        .then(() => {
-                            this.device = device
-                            this.fire(VR_DETECTED,{})
-                        })
-                        .catch(printError);
-
-                }).catch(printError);*/
+            if ( 'xr' in navigator && 'supportsSession' in navigator.xr ) {
+                navigator.xr.supportsSession( 'immersive-vr' ).then(()=>{
+                    console.log("immersive is supported");
+                    elem.disabled = false
+                }).catch((e)=>{
+                    console.error(e)
+                });
             } else {
                 console.log("does not have webxr");
             }
@@ -50,9 +69,10 @@ WebXRSystem.queries = {
         }
     },
     active: {
-        components: [WebXRMode],
+        components: [WebXRActive],
         listen: {
             added:true,
+            removed:false,
         }
     }
 }
