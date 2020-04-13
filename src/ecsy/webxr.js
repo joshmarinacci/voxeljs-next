20bcf8df2ce9e18d10d3cb6a98b5ae293f3da4b5
josh <joshua@marinacci.org>
2020-04-13 15:21:04 -0700

    disable the webxr button if not supported

--- src/ecsy/webxr.js
+++ src/ecsy/webxr.js
@@ -10,17 +10,35 @@ export class WebXRButton extends Component {
 export class WebXRSystem extends System {
     execute(delta, time) {
         this.queries.buttons.added.forEach(ent => {
-            console.log("butotn added");
+            console.log("button added");
             let elem = document.createElement('button')
             elem.innerText = "webxr"
             elem.classList.add("webxr")
-            elem.addEventListener('click',(e)=>{
+            elem.addEventListener('click', (e) => {
                 e.stopPropagation()
                 e.preventDefault()
                 ent.addComponent(WebXRButton)
             })
+            elem.disabled = true
             document.documentElement.append(elem)
-            console.log("added the element",elem)
+            console.log("added the element", elem)
+
+            if ('xr' in navigator) {
+                console.log("has webxr")
+                elem.disabled = false
+                /*
+                navigator.xr.requestDevice().then((device) => {
+                    device.supportsSession({immersive: true})
+                        .then(() => {
+                            this.device = device
+                            this.fire(VR_DETECTED,{})
+                        })
+                        .catch(printError);
+
+                }).catch(printError);*/
+            } else {
+                console.log("does not have webxr");
+            }
         })
     }
 }
